50279ddfadf78fe837547c3b89edd62c4c039b06
Albert Ziegenhagel <albert.ziegenhagel@scai.fraunhofer.de>
2018-04-17 17:07:12 +0200

    [msys] Update to 20170918

--- scripts/cmake/vcpkg_acquire_msys.cmake
+++ scripts/cmake/vcpkg_acquire_msys.cmake
@@ -49,20 +49,18 @@ function(vcpkg_acquire_msys PATH_TO_ROOT_OUT)
   if(_vam_HOST_ARCHITECTURE STREQUAL "AMD64")
     set(TOOLSUBPATH msys64)
     set(URLS
-      "http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20161025.tar.xz"
-      "https://sourceforge.net/projects/msys2/files/Base/x86_64/msys2-base-x86_64-20161025.tar.xz/download"
+      "https://sourceforge.net/projects/msys2/files/Base/x86_64/msys2-base-x86_64-20170918.tar.xz/download"
     )
-    set(ARCHIVE "msys2-base-x86_64-20161025.tar.xz")
-    set(HASH 6c4c18ec59db80b8269698d074866438a624f1ce735ee5005a01b148b02e8f2e966ae381aa1cb4c50f6226c3b7feb271e36907cf26580df084d695b3c9f5c0eb)
+    set(ARCHIVE "msys2-base-x86_64-20170918.tar.xz")
+    set(HASH e302afb2b352ef012fdf39fdb8ceaa7a2a36c6f36104d740b57197fe40bd2cb4f12cb96909ac38716f900aaefc6bd9c8548ca6c9c9af6f465b03990a4fe8e6f7)
     set(STAMP "initialized-msys2_64.stamp")
   else()
     set(TOOLSUBPATH msys32)
     set(URLS
-      "http://repo.msys2.org/distrib/i686/msys2-base-i686-20161025.tar.xz"
-      "https://sourceforge.net/projects/msys2/files/Base/i686/msys2-base-i686-20161025.tar.xz/download"
+      "https://sourceforge.net/projects/msys2/files/Base/i686/msys2-base-i686-20170918.tar.xz/download"
     )
-    set(ARCHIVE "msys2-base-i686-20161025.tar.xz")
-    set(HASH c9260a38e0c6bf963adeaea098c4e376449c1dd0afe07480741d6583a1ac4c138951ccb0c5388bd148e04255a5c1a23bf5ee2d58dcd6607c14f1eaa5639a7c85)
+    set(ARCHIVE "msys2-base-i686-20170918.tar.xz")
+    set(HASH 2311cd96b243a0a4adcf9bb39ece4edc14ff1859bc4d96c03d7a8d9cb0849d1d546d346648b5b8f76c438408bc6d41f51f075ffbc077a2a56e45b7d771091d33)
     set(STAMP "initialized-msys2_32.stamp")
   endif()
 
